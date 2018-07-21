801ade9f8d7d7dbc8faabc0f7b2c6634c150681d
Ovyerus <iamovyerus@gmail.com>
2018-07-21 22:11:25 +1000

    Extend user routes

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -3,19 +3,40 @@
  * Licensed under BSD-3-Clause
  */
 
-import {Request, Response, Router} from 'express';
+import {NextFunction, Request, Response, Router} from 'express';
 
 import IUser, {convertFromDB} from '../../models/api/IUser';
 import User from '../../models/db/User';
 import {handlePagination} from '../queryHandlers';
 
 const router = Router();
+const selfRelations = [''];
+const publicRelations = [''];
 
 router.get('/', (req: Request, res: Response) => {
-    const users = handlePagination<User>(req, res, User);
+    let users = handlePagination<User>(req, res, User);
+    users = users.map(convertFromDB);
 
     res.json(users);
 });
+
+router.route('/@me')
+    .all(async (req: Request, res: Response, next: NextFunction) => {
+        if (!res.locals.authenticated) res.sendStatus(404);
+        else next();
+    })
+    .get(async (req: Request, res: Response) => {
+        let user = await User.findOne({id: res.locals.user.id});
+        user = convertFromDB(user);
+
+        res.json(user);
+    })
+    .patch(async (req: Request, res: Response) => {
+
+    })
+    .delete(async (req: Request, res: Response) => {
+
+    })
 
 router.route('/:user')
     .get(async (req: Request, res: Response) => {
