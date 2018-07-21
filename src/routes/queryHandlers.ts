c8e4b2afa6680a8fe2a74b84df2b66c72dc5be53
Ovyerus <iamovyerus@gmail.com>
2018-07-21 22:08:35 +1000

    Update handlePagination

--- src/routes/queryHandlers.ts
+++ src/routes/queryHandlers.ts
@@ -14,18 +14,22 @@ import {BaseEntity} from 'typeorm';
  * @param target The active record class to paginate through. Should be the uninstantiated constructor object for T.
  * @returns Paginated array of T.
  */
-export function handlePagination<T extends BaseEntity>(req: Request, res: Response, target: T): T[] {
-    /*
-     * Available pagination options:
-     *
-     * ?limit=number - Limits the amount of results from the API. Default: 20, Max: 100.
-     * ?page=number - How many pages of `limit` results to skip. Default: 1.
-     * ?filter=field1,value1,...,fieldN,valueN - Filter to pass results under, in an AND operation. Return an error if not even length.
-     * ?filterOr=field1,value1,...,fieldN,valueN - Same as `?filter` but an OR operation instead. Return an error if not even length.
-     * ?fuzzy=field1,value1,...,fieldN,valueN - Same as `?filter` but fuzzy matching instead of exact. Return an error if not even length.
-     * ?fuzzyOr=field1,value1,...,fieldN,valueN - Same as `?filterOr` but fuzzy matching instead of exact. Return an error if not even length
-     */
-    target.find();
+export function handlePagination<T extends BaseEntity>(req: Request, res: Response, target: T, relations?: string[]): T[] {
+    const limit = Math.min(Number(req.query.limit || 20), 100);
+    const page = Number(req.query.page || 1);
+    const ret: T[] = target.find({take: limit, skip: page * limit, relations});
 
     return [];
 }
+
+// for future reference
+/*
+ * Available pagination options:
+ *
+ * ?limit=number - Limits the amount of results from the API. Default: 20, Max: 100.
+ * ?page=number - How many pages of `limit` results to skip. Default: 1.
+ * ?filter=field1,value1,...,fieldN,valueN - Filter to pass results under, in an AND operation. Return an error if not even length.
+ * ?filterOr=field1,value1,...,fieldN,valueN - Same as `?filter` but an OR operation instead. Return an error if not even length.
+ * ?fuzzy=field1,value1,...,fieldN,valueN - Same as `?filter` but fuzzy matching instead of exact. Return an error if not even length.
+ * ?fuzzyOr=field1,value1,...,fieldN,valueN - Same as `?filterOr` but fuzzy matching instead of exact. Return an error if not even length
+ */
