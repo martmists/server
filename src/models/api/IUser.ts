71ae189dcde484e43f793f2c9a70809ba2cececa
sr229 <aguilarralph13@gmail.com>
2018-08-24 22:59:26 +0800

    rewrite to use async

--- src/models/api/IUser.ts
+++ src/models/api/IUser.ts
@@ -46,10 +46,15 @@ export function convertFromDB(obj: User): IUser {
  * @param password password to check if salt exists. 
  * @param saltedHash the salted hash saved in your database.
  */
-export function verifyPassword(password: string, saltedHash: string) {
+export async function verifyPassword(password: string, saltedHash: string) {
     // first lets hash the resulting password to compare them both
-   bcrypt.hash(password, 25, (err, hash) => {
-           if (!bcrypt.compareSync(password, saltedHash)) throw new Error('password does not match stored salted password. Will not auth.');
-           else return; // do nothing atm.
-    });
+    let match = await bcrypt.compare(password, saltedHash);
+
+    if (!match) {
+        await JSON.stringify({consentAuth: false});
+        throw new Error('password does not match. Do not auth');
+    } else {
+        await JSON.stringify({consentAuth: true});
+        console.log('password matches. Authentication consent granted.');
+    }
 }
