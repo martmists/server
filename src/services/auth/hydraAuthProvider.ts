1cb2ac4c69b891956ef4d1d44c758ae45ce4aeb9
sr229 <aguilarralph13@gmail.com>
2018-08-25 21:39:48 +0800

    rename back to hydraAuthProvider and complete impl

--- src/services/auth/hydraAuthProvider.ts
+++ src/services/auth/hydraAuthProvider.ts
@@ -7,33 +7,48 @@
 //  since we still require a handbuilt function just to query with Hydra, this
 //  was created.
 
-import csrf = require('csurf');
-
 import { request } from '../../helpers/request';
 
 const hydraEndpoint = process.env.HYDRA_ENDPOINT;
 
-export default class HydraConsentProvider {
- public static getLoginRequest(challenge) {
+export default class HydraAuthProvider {
+   public static getLoginRequest(challenge) {
       return this.get('login', challenge);
-   }
- public static acceptLoginRequest(challenge, body) {
+    }
+
+   public static acceptLoginRequest(challenge, body) {
      return this.put('login', 'accept', challenge, body);
- }
+    }
+
+   public static rejectLoginRequest(challenge, body) {
+       return this.put('login', 'reject', challenge, body);
+   }
+
+   public static getConsentRequest(challenge) {
+       return this.get('consent', challenge);
+   }
+
+   public static acceptConsentRequest(challenge, body) {
+     return this.put('consent', 'accept', challenge, body);
+   }
+
+   public static rejectConsentRequest(challenge, body) {
+     return this.put('consent', 'reject', challenge, body);
+   }
  /**
   * gets a specific flow and challenge from ORY Hydra
   * @param flow the auth flow to use. Can be a valid OAuth2 provider call.
   * @param challenge the challenge to return to the consumer.
   * @param action the action to fulfill by the HYDRA server.
   */
- private static get(flow, challenge) {
-     request('GET', `${hydraEndpoint}/oauth2/auth/requests/${flow}/${challenge}`, undefined, {})
+   private static get(flow, challenge) {
+     request('GET', `${hydraEndpoint}/oauth2/auth/requests/${flow}/${challenge}`, undefined, '')
       .then(res => res)
       .catch(err => new Error(`Failed to get flow ${flow} from ${hydraEndpoint}.\n ${err.stack}`));
-  }
- private static put(flow, action, challenge, body) {
+    }
+   private static put(flow, action, challenge, body) {
      request('PUT', `${hydraEndpoint}/oauth2/auth/requests/${flow}/${challenge}/${action}`, {headers: { 'Content-Type': 'application/json'}}, JSON.stringify(body))
      .then(res => res)
      .catch(err => new Error(`Failed to put flow ${flow} from ${hydraEndpoint}.\n ${err.stack}`));
-  }
+    }
 }
